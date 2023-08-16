8f7078b16426a4e5fc51098760adf83a85e489b4
Sergio <harogio101@gmail.com>
2023-08-15 19:35:01 -0700

    Alter original grid height/width

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 let container = document.querySelector('#container');
-for (let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        let div = document.createElement('div');
-        div.className = "grid";
-        container.appendChild(div);
+let sizeButton = document.querySelector('#size');
+let size = 0;
+let grids = 0;
 
+for (let i = 0; i < 100; i++){
+    let div = document.createElement('div');
+    div.classList.add('grid');
+    container.appendChild(div);
+};
+
+sizeButton.addEventListener('click', function () {
+    size = prompt("What would you like your grid size by? Enter one number, it will automatically add the second. Ex: 10 -> 10x10");
+    if(size > 100){
+        size = 100;
     }
+    size = parseInt(size);
+
+    let divs = document.querySelectorAll('.grid');
+
+    divs.forEach((divi) => {
+        divi.remove();
+    });
+
+    makeNewGrid();
+});
+
+function makeNewGrid(){
+
+let boxSize = (Math.sqrt(902500/(sizesize))) - 2;
+
+for (let i = 0; i < sizesize; i++){
+    let div = document.createElement('div');
+    div.classList.add('grid');
+    div.setAttribute('style', 'min-width: ' + boxSize + "px; min-height: " + boxSize + 'px; max-width: ' + boxSize + "px; max-height: " + boxSize + "px;");
+    container.appendChild(div);
+};
+
+grids = document.querySelectorAll(".grid");
+
+grids.forEach(grid => grid.addEventListener('mouseover', () => {
+    grid.style.background = "black";
+}));
+
+
 }
 
-let boxes = document.querySelectorAll('.grid');
+grids = document.querySelectorAll(".grid");
 
-boxes.forEach(box => box.addEventListener('mouseover', () => {
-    box.style.cssText = "background-color: black;";
-}))
-
-
-
+grids.forEach(grid => grid.addEventListener('mouseover', () => {
+    grid.style.background = "black";
+}));
 
